cc8836f4c60879e1fdff038f94a0c3f5501d50d1
Burak İlgün <burak.ilgun@outlook.com>
2020-03-19 00:03:34 +0300

    admin articles pages added and routing modified.

--- src/app/admin-pages/admin.module.ts
+++ src/app/admin-pages/admin.module.ts
@@ -10,11 +10,21 @@ import { ComponentsModule } from '../components/components.module';
 //Components
 import { AdminLayoutComponent } from '../layout/admin-layout/admin-layout.component';
 import { AdminNavComponent } from '../nav/admin-nav/admin-nav.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { ArticleAddComponent } from './article/article-add/article-add.component';
+import { ArticleUpdateComponent } from './article/article-update/article-update.component';
+import { ArticleListComponent } from './article/article-list/article-list.component';
+import { AdminArticleComponent } from './article/article/article.component';
 
 @NgModule({
   declarations: [
     AdminLayoutComponent,
-    AdminNavComponent
+    AdminNavComponent,
+    AdminHomeComponent,
+    AdminArticleComponent,
+    ArticleAddComponent,
+    ArticleUpdateComponent,
+    ArticleListComponent
   ],
   imports: [
     CommonModule,
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
-import { ArticleComponent } from './pages/article/article.component';
 import { CategoryArticlesComponent } from './pages/category-articles/category-articles.component';
 import { SearchComponent } from './pages/search/search.component';
 import { ArchiveComponent } from './pages/archive/archive.component';
+import { AdminHomeComponent } from './admin-pages/admin-home/admin-home.component';
+import { AdminArticleComponent } from './admin-pages/article/article/article.component';
+import { ArticleListComponent } from './admin-pages/article/article-list/article-list.component';
+import { ArticleAddComponent } from './admin-pages/article/article-add/article-add.component';
+import { ArticleUpdateComponent } from './admin-pages/article/article-update/article-update.component';
+import { ArticleComponent } from './pages/article/article.component';
 
 
 const routes: Routes = [
@@ -60,7 +65,39 @@ const routes: Routes = [
   },
   {
     path: "admin",
-    component: AdminLayoutComponent
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: "",
+        component: AdminHomeComponent
+      },
+      {
+        path: "anasayfa",
+        component: AdminHomeComponent
+      },
+      {
+        path: "makale",
+        component: AdminArticleComponent,
+        children: [
+          {
+            path: "",
+            component: ArticleListComponent
+          },
+          {
+            path: "liste",
+            component: ArticleListComponent
+          },
+          {
+            path: "ekle",
+            component: ArticleAddComponent
+          },
+          {
+            path: "guncelle/:id",
+            component: ArticleUpdateComponent
+          }
+        ]
+      }
+    ]
   }
 ];
 
